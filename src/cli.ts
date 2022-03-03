97b4cc46033a98c3858f168dced85354da0ab4d5
WidgetKing <tristan.ward@infosemantics.com.au>
2022-03-03 04:21:24 +0000

    Adding extra scripts

---
+++ src/cli.ts
@@ -0,0 +1,3 @@
+// Code in here will be the entry point for any command line interface this project is creating.
+// That said, if we're making a Captivate library, it's unlikely there will be any command line interface.
+// Therefore, don't feel too bad about deleting this file and the 'cli' script from the packages.json
---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+// This is a placeholder test to confirm jest is working out of the box.
+// Feel free to delete and add your own unit tests here!
+import add from "./index";
+
+test('Test placeholder add function', () => {
+	expect(add(1,2)).toBe(3);
+	expect(add(10,20)).toBe(30);
+})
